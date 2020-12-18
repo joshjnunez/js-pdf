89aa7fa738817e9c476be69e0e14a2c58aab1deb
Josh Nunez <joshjnunez09@gmail.com>
2020-12-18 15:53:20 -0600

    jspdf

--- components/App.js
+++ components/App.js
@@ -1,54 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import '../Style.css';
-const { characters } = require('../characters.js');
 
+const { jsPDF } = require("jspdf");
 const App = () => {
-  const [time, setTime] = useState('');
-  const [character, setCharacter] = useState(characters[0].gif);
-  const [alarmTime, setAlarmTime] = useState('');
+  // const [time, setTime] = useState('');
+  // const [character, setCharacter] = useState(characters[0].gif);
+  // const [alarmTime, setAlarmTime] = useState('');
 
-  useEffect(() => {
-    const interval = setInterval(
-      () => setTime(new Date().toLocaleTimeString()),
-      1000
-    );
-    // console.info(characters);
-    let index = 1;
-    const changeChars = setInterval(() => {
-      if (index === 9) {
-        setCharacter(characters[0].gif);
-        index = 0;
-      }
-      setCharacter(characters[index++].gif);
-    }, 10000);
 
-    return () => {
-      clearInterval(interval);
-      clearInterval(changeChars);
-    };
-  }, []);
+  const createPDF = () => {
 
-  const setAlarm = () => {
-    console.info('clicked');
+    if(document.getElementById("name").value == "" || document.getElementById("number").value == ""){
+      alert("Please enter all fields");
+    } else {
+
+      const doc = new jsPDF();
+      
+      doc.text(document.getElementById("name").value, 10, 10);
+      doc.text(document.getElementById("number").value, 25, 25);
+      doc.save("output.pdf");
+
+    }
   };
 
-  const handleChange = (e) => {
-    setAlarmTime(e.target.value);
-    console.log(e.target.value);
-  };
+
 
   return (
-    <div className="app">
-      <h1>New Girl Clock</h1>
-      <p>It's always time for New Girl</p>
-      <div className="time">{time}</div>
-      <img src={character} />
-      <h3>Set ALARM:</h3>
-      <input onChange={(e) => handleChange(e)} placeholder="00:00"></input>
-      <button size="lg" onClick={() => setAlarm()}></button>
-
-      {/* <div>{alarmTime}</div> */}
+    <div>
+      <input id="name"></input>
+      <input id="number"></input>
+      <button onClick={()=> createPDF()}>SUBMIT</button>
     </div>
+
+
   );
 };
 export default App;
